cb1f8268e4316a5225a12a70ebcb5057c4f9cb47
SinghSomnath <somnath.rcc@gmail.com>
2025-05-11 23:27:59 +0530

    #133

--- GettingStarted.py
+++ GettingStarted.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 load_dotenv()
 
 os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
@@ -11,3 +15,14 @@ os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
 
 llm=ChatOpenAI(model="gpt-4o")
 print(llm)
+
+loader = WebBaseLoader("https://docs.smith.langchain.com/administration/tutorials/manage_spend");
+docs = loader.load();
+print(docs)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings()
+
+vectorstoredb = FAISS.from_documents(documents, embeddings)
